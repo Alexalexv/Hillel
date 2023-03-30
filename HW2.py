6ed3a9289ade5a6f709567404702ccdcf41155aa
o.vasylenko <schmidtik4@gmail.com>
2023-03-30 23:24:18 +0300

    HW2, HW3, HW4 has been added

---
+++ HW2.py
@@ -0,0 +1,2 @@
+cats = ['Murzik', 'Barsik', 'Pantera']
+print(cats[0], cats[1], cats[2], sep=', ')
---
+++ HW3.py
@@ -0,0 +1,5 @@
+countries = ['Ukraine', 'Poland', 'Germany']
+countries_capitals = {countries[0]: 'Kyiv', countries[1]: 'Warsaw', countries[2]: 'Berlin'}
+# соррі, тут так
+for key, value in countries_capitals.items():
+    print(f"{key}: {value}")
