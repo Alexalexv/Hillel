faabb9e1988e2a2c152253e53b8b63b4e058896d
o.vasylenko <schmidtik4@gmail.com>
2023-04-05 12:24:30 +0300

    upd HW9

--- HW9.py
+++ HW9.py
@@ -5,4 +5,8 @@ for i in range(1, n + 1):
     right = list(range(i - 1, 0, -1))
     left_str = ' '.join(map(str, left))
     right_str = ' '.join(map(str, right))
-    print('  '*(n-i)+left_str, right_str)
+    print('  '*(n-i)+left_str, end='')
+    if len(right_str):
+        print(' '+right_str)
+    else:
+        print()
