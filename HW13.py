ba3a80dc78655a7d08e47d6546f8ca8e3f2498cd
o.vasylenko <schmidtik4@gmail.com>
2023-04-08 00:28:36 +0300

    upd HW13

--- HW13.py
+++ HW13.py
@@ -4,7 +4,7 @@ if user_input.count('@') != 1 or user_input.count('.') != 1:
     print(f'Please, check symbols "@" or "." in yours email "{user_input}"')
 elif user_input.index('@') > user_input.index('.'):
     print(f'Symbols "@" and "." has wrong order in yours email "{user_input}"')
-elif user_input[0] != '@' or user_input[-1] != '.':
+elif user_input[0] == '@' or user_input[-1] == '.':
     print(f'Your email "{user_input}" starts or ends with characters "@" or "."')
 else:
     print(f'Our script is not perfect, but perhaps your email "{user_input}" is valid')
