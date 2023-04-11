bba4ddc158a898f5c250d98bf71ec125043a3b19
o.vasylenko <schmidtik4@gmail.com>
2023-04-11 23:49:44 +0300

    HW15

---
+++ HW15.py
@@ -0,0 +1,13 @@
+mini = int(input('Enter MIN: '))
+maxi = int(input('Enter MAX: '))
+lst = [2, 4, 6, 2, 1, 1, 9, 4, 6]
+filtered_list = [i for i in lst if mini <= i <= maxi]
+sum_ = sum(filtered_list)
+k = 1
+product = 1
+for i in filtered_list:
+    if sum_ == 0:  # for cases when no elements between mini and maxi
+        product = 0
+    else:
+        product = product * i * k  # k was added for cases where between mini and maxi only one element (another lst needed)
+print(f'sum_ = {sum_}, product = {product}')
