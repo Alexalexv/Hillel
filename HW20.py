0a66859fedc666cf427d149ef4ea6d35fa167ae5
o.vasylenko <schmidtik4@gmail.com>
2023-04-15 01:12:38 +0300

    upd HW20

--- HW20.py
+++ HW20.py
@@ -1,8 +1,10 @@
 len_ = 0
+str_ = ''
 with open('file', 'r') as f:
     for line in f:
         line_len = len(line)
         if line_len > len_:
             len_ = line_len
+            str_ = line
 
-print(f'Max length of line in file is {len_}')
+print(str_)
