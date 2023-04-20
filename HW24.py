56783a72fabd0ce600dc7468b19c1c4d8b582f22
o.vasylenko <schmidtik4@gmail.com>
2023-04-21 00:58:58 +0300

    HW22

--- HW24.py
+++ HW24.py
@@ -1,11 +1,17 @@
 def second_largest_number(lst):
     if len(lst):
-        max_ = lst[0]
-        less = lst[0]
+        max_ = float('-inf')
+        less = float('-inf')
         for i in lst:
             if i > max_:
                 less = max_
                 max_ = i
+            elif i > less:
+                less = i
         return less
     else:
         return None
+
+
+lst_ = [-150, 4, 2, 1, 5, 2, 5,  7]
+print(second_largest_number(lst_))
