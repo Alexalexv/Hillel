24fa2ef0dad00e4c15814176b4c9b7e4c7c9b928
o.vasylenko <schmidtik4@gmail.com>
2023-04-26 01:35:32 +0300

    HW27

--- HW27.py
+++ HW27.py
@@ -1,6 +1,8 @@
 def gen(m, n):
     for i in range(1, n + 1):
         yield i
+    for i in range(1, n + 1):
+        yield i ** 2
     exp = 2
     for i in range(1, n + 1):
         if exp <= m:
@@ -8,3 +10,7 @@ def gen(m, n):
             exp += 1
         else:
             yield i ** m
+
+
+for i in gen(3, 4):
+    print(i)
