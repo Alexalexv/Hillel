9b238c5fc0fe41674c12f85b7c68d22b6af818d0
o.vasylenko <schmidtik4@gmail.com>
2023-04-28 01:50:08 +0300

    HW30

---
+++ HW30.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import datetime as td
+
+l_count = 32
+first_l = '27/03/2023 19:15'
+
+diff_1 = td.timedelta(days=3)
+diff_2 = td.timedelta(days=4)
+date = datetime.strptime(first_l, '%d/%m/%Y %H:%M')
+counter = 1
+
+while counter <= l_count:
+    print(date.strftime(f'Lecture  {counter:2}: %d %b %Y %H:%M'))
+    counter += 1
+    date += diff_1
+    print(date.strftime(f'Lecture  {counter:2}: %d %b %Y %H:%M'))
+    counter += 1
+    date += diff_2
