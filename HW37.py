eaa9e37db830e195f130bd7840413dc21e7595da
o.vasylenko <schmidtik4@gmail.com>
2023-05-16 23:07:23 +0300

    HW37

---
+++ HW37.py
@@ -0,0 +1,21 @@
+class AttributePrinterMixin:
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        str_ = f'{class_name}: {{\n'
+        for k, v in self.__dict__.items():
+            k = k.replace('_A__', '')
+            k = k.replace('_', '')
+            str_ = str_ + f'\t{k}: ' + f'{v}\n'
+        str_ = str_ + '}'
+        return str_
+
+
+class A(AttributePrinterMixin):
+    def __init__(self):
+        self.public_filed = 3
+        self._protected_field = 'q'
+        self.__private_field = [1, 2, 3]
+
+
+a = A()
+print(a)
