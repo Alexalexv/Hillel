50c9f38ac167f8200c575755146045b1a29c9e37
o.vasylenko <schmidtik4@gmail.com>
2023-05-17 11:05:14 +0300

    HW37

--- HW37.py
+++ HW37.py
@@ -1,9 +1,12 @@
+import re
+
+
 class AttributePrinterMixin:
     def __repr__(self):
         class_name = self.__class__.__name__
         str_ = f'{class_name}: {{\n'
         for k, v in self.__dict__.items():
-            k = k.replace('_A__', '')
+            k = re.sub('^_[A-Za-z]+__', '', k)
             if k[0] == '_':
                 k = k[1:]
             str_ = str_ + f'\t{k}: ' + f'{v}\n'
@@ -11,12 +14,12 @@ class AttributePrinterMixin:
         return str_
 
 
-class A(AttributePrinterMixin):
+class B:
     def __init__(self):
-        self.public_filed = 3
-        self._protected_field = 'q'
-        self.__private_field = [1, 2, 3]
+        self.__private_in_B = 'private_in_B'
 
+class A(B, AttributePrinterMixin):
+    pass
 
 a = A()
 print(a)
