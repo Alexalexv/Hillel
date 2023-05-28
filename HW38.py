ecfd6351bec3c8d28729fa3eab8800a219979225
o.vasylenko <schmidtik4@gmail.com>
2023-05-28 14:54:58 +0300

    HW38

--- HW38.py
+++ HW38.py
@@ -4,7 +4,9 @@ class ClassCache:
         self.func_dict = {}
 
     def __call__(self, *args, **kwargs):
-        key_dict = str(args) + str(kwargs)
+        key_dict = str(args)
+        key_kwargs = sorted(kwargs)
+        key_dict = key_dict + str(key_kwargs)
         if key_dict in self.func_dict.keys():
             result = self.func_dict[key_dict]
         else:
